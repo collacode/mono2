e188c11f2a863a86b1e5911234b74b9ce1e09767
Seongbin Kim <seongbin9786@gmail.com>
2023-07-14 18:03:18 +0900

    TSConfig 공통화

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: false,
-    coverage: {
-      reporter: ["text", "html"],
+    plugins: [react()],
+    test: {
+        globals: false,
+        coverage: {
+            reporter: ['text', 'html'],
+        },
+        environment: 'jsdom',
+        include: ['**/*.test.tsx'],
     },
-    environment: "jsdom",
-    include: ["**/*.test.tsx"],
-  },
 });
