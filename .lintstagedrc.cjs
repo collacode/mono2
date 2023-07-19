f644e6fe1f007fe12bda35cad49e6d7541c01a42
Seongbin Kim <seongbin9786@gmail.com>
2023-07-19 10:24:55 +0900

    fix: lint 문제 종료

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,16 +1,13 @@
-const path = require('path')
+const path = require('path');
  
 const buildEslintCommand = (filenames) =>
   `yarn f run next lint --fix --file ${filenames
-    .map((f) => {
-      console.log(path.relative(process.cwd(), f));
-      return path.relative(process.cwd(), f)
-    })
-    .join(' --file ')}`
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
 
 module.exports = {
   "./api/**/*.{ts,tsx}": "yarn a lint",
   "./apispec/**/*.{ts,tsx}": "yarn as lint",
   "./components/**/*.{ts,tsx}": "yarn c lint",
   "./frontend/**/*.{ts,tsx}": [buildEslintCommand],
-}
+};
