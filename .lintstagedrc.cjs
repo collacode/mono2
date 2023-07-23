8d59e31835502aeb5b22e7d05895983fd7bbcee5
Seongbin Kim <seongbin9786@gmail.com>
2023-07-23 20:03:54 +0900

    style: staged lint 된 거 추가

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,9 @@
 const path = require('path');
 
-const buildEslintCommand = (filenames) => `yarn f run next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+const buildEslintCommand = (filenames) =>
+    `yarn f run next lint --fix --file ${filenames
+        .map((f) => path.relative(process.cwd(), f))
+        .join(' --file ')}`;
 
 // eslint가 실행하는 prettier는 eslint가 지원하는 확장자에만 실행되므로, prettier도 실행해줘야 함
 // Root 폴더는 Root 폴더에 속한 파일에 대해서 prettier만 실행
