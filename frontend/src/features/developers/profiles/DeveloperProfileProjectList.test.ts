420e76ebd359cb2030b0f8512762c12f6f26a53f
Seongbin Kim <seongbin9786@gmail.com>
2023-08-07 16:43:45 +0900

    test: 단위 테스트 예시 추가

---
+++ frontend/src/features/developers/profiles/DeveloperProfileProjectList.test.ts
@@ -0,0 +1,10 @@
+import { describe, test } from 'vitest';
+
+describe.skip('개발자 프로필 프로젝트 목록 컴포넌트 단위 테스트', () => {
+    // Presentational 사용
+    test('정보 표시', async () => {});
+
+    // Container 사용
+    test('포지션 선택 Select에서 Option 선택 시 해당 포지션 기준의 페이지로 이동', async () => {});
+    test('프로젝트 링크 클릭 시 해당 프로젝트 프로필 페이지로 이동', async () => {});
+});
